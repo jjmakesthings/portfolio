78f17f293c407ded42e781752a7b8f21fb5262e8
John <megaman5249@gmail.com>
2021-12-10 20:39:08 -0500

    Add gitignore, init, prettier

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
-
-const navBar = (function(){
-    const wrapper = document.getElementById("nav-wrap");
-    wrapper.addEventListener("mouseenter", (e)=>{
-        if (e.target === wrapper){
-            let popoutLength = (window.scrollY > 45)?45:window.scrollY;
-            e.target.style.transform = `translate(0,${popoutLength}px)`;
-        };
-    });
-    wrapper.addEventListener("mouseleave", (e)=>{
-        if (e.target === wrapper){
-            e.target.style.transform = "";
-        };
-    });
-    document.addEventListener("scroll", (e)=>{
-        wrapper.style.transform = "";
-    });
-    return{};
-}());
+const navBar = (function () {
+  const wrapper = document.getElementById("nav-wrap");
+  wrapper.addEventListener("mouseenter", (e) => {
+    if (e.target === wrapper) {
+      let popoutLength = window.scrollY > 45 ? 45 : window.scrollY;
+      e.target.style.transform = `translate(0,${popoutLength}px)`;
+    }
+  });
+  wrapper.addEventListener("mouseleave", (e) => {
+    if (e.target === wrapper) {
+      e.target.style.transform = "";
+    }
+  });
+  document.addEventListener("scroll", (e) => {
+    wrapper.style.transform = "";
+  });
+  return {};
+})();
